4726744927a66f89770e507d41f2d5f873735a6d
Wafaa Safar <wafaasafar97@gmail.com>
2017-11-01 22:07:50 -0400

    First commit

---
+++ CityYearJaxDashboard/Controllers/EnrollmentsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CityYearJaxDashboard.Views.Shared;
+using System.IO;
+
+namespace CityYearJaxDashboard.Controllers
+{
+    public class EnrollmentsController : Controller
+    {
+        private CYJDashboardEntities db = new CYJDashboardEntities();
+
+        // GET: Enrollments
+        public ActionResult Index()
+        {
+            return View(db.Enrollments.ToList());
+        }
+
+        // GET: Enrollments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Enrollments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Enrollment_ID,AnnualGoal,BestofCYGoal,Q1Goal,Q1Actual,Q2Goal,Q2Actual,Q3Goal,Q3Actual,Q4Goal,Q4Actual")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Enrollments.Add(enrollment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // POST: Enrollments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Enrollment_ID,AnnualGoal,BestofCYGoal,Q1Goal,Q1Actual,Q2Goal,Q2Actual,Q3Goal,Q3Actual,Q4Goal,Q4Actual")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(enrollment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // POST: Enrollments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Enrollment enrollment = db.Enrollments.Find(id);
+            db.Enrollments.Remove(enrollment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

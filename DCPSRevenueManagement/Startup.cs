6eb2422a7003e03fb19f4f40b56f7bad40b225b2
Eric Mwaura <ericmuhungi@gmail.com>
2017-11-02 22:50:41 -0400

    DCPSRevenueManagement

---
+++ DCPSRevenueManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DCPSRevenueManagement.Startup))]
+namespace DCPSRevenueManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
